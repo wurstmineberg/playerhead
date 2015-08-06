a03d70b166a8413dc95b8ce2c12c8300304f9dc9
Fenhl <fenhl@fenhl.net>
2015-08-06 06:58:19 +0000

    Improve error handling

--- playerhead.py
+++ playerhead.py
@@ -37,13 +37,17 @@ import shutil
 import struct
 import subprocess
 import sys
+import time
+import traceback
 import uuid
 
 def check_nick(player):
     return bool(re.match('[A-Za-z0-9_]{1,16}$', player))
 
-def head(player, hat=True, profile_id=None):
-    player_skin = skin(player, profile_id=profile_id)
+def head(player, hat=True, profile_id=None, error_log=None):
+    if error_log is None:
+        error_log = sys.stderr
+    player_skin = skin(player, profile_id=profile_id, error_log=error_log)
     if hat:
         return Image.alpha_composite(player_skin.crop((8, 8, 16, 16)), player_skin.crop((40, 8, 48, 16)))
     return player_skin.crop((8, 8, 16, 16))
@@ -56,13 +60,40 @@ def java_uuid_hash_code(uuid):
     m2 = (mostSigBits & 0xFFFFFFFF00000000) >> 32
     return (l1 ^ l2) ^ (m1 ^ m2)
 
-def skin(player, profile_id=None):
+def retry_request(url, error_log=None, *args, **kwargs):
+    if error_log is None:
+        error_log = sys.stderr
+    response = requests.get(url, *args, **kwargs)
+    if response.status_code == 429:
+        print('Rate limited, trying again in a second', file=error_log)
+        time.sleep(1)
+        response = requests.get(url, *args, **kwargs)
+        if response.status_code == 429:
+            print('Still rate limited, trying again in 10 seconds', file=error_log)
+            time.sleep(10)
+            response = requests.get(url, *args, **kwargs)
+            if response.status_code == 429:
+                print('Still rate limited, trying again in a minute', file=error_log)
+                time.sleep(60)
+                response = requests.get(url, *args, **kwargs)
+                if response.status_code == 429:
+                    print('Still rate limited, giving up', file=error_log)
+    response.raise_for_status()
+    return response
+
+def skin(player, profile_id=None, error_log=None):
+    if error_log is None:
+        error_log = sys.stderr
     if profile_id is None:
         response = requests.get('https://api.mojang.com/users/profiles/minecraft/{}'.format(player))
         response.raise_for_status()
-        profile_id = uuid.UUID(response.json()['id'])
-    response = requests.get('https://sessionserver.mojang.com/session/minecraft/profile/{}'.format(re.sub('-', '', str(profile_id))))
-    response.raise_for_status()
+        try:
+            j = response.json()
+        except ValueError:
+            print('Failed to decode response: {!r}'.format(response), file=error_log)
+            raise
+        profile_id = uuid.UUID(j['id'])
+        response = retry_request('https://sessionserver.mojang.com/session/minecraft/profile/{}'.format(re.sub('-', '', str(profile_id))), error_log=error_log)
     textures = json.loads(base64.b64decode(response.json()['properties'][0]['value'].encode('utf-8')).decode('utf-8'))['textures']
     if 'SKIN' not in textures:
         # default skin
@@ -76,18 +107,23 @@ def skin(player, profile_id=None):
     return Image.open(response.raw)
 
 def write_head(player, target_dir=None, size=8, filename=None, error_log=None, profile_id=None, hat=True):
-    if target_dir is None:
-        target_dir = pathlib.Path()
-    if isinstance(target_dir, str):
-        target_dir = pathlib.Path(target_dir)
-    if error_log is None:
-        error_log = sys.stderr
-    if not check_nick(player):
-        print('Invalid player name: ' + player, file=error_log)
+    try:
+        if target_dir is None:
+            target_dir = pathlib.Path()
+        if isinstance(target_dir, str):
+            target_dir = pathlib.Path(target_dir)
+        if error_log is None:
+            error_log = sys.stderr
+        if not check_nick(player):
+            print('Invalid player name: ' + player, file=error_log)
+            return False
+        if not target_dir.exists():
+            target_dir.mkdir(parents=True)
+        head(player, hat=hat, profile_id=profile_id, error_log=error_log).resize((size, size)).save(str(target_dir / ((player if filename is None else filename) + '.png')))
+    except Exception:
+        print('Error writing head for {}'.format(player), file=error_log)
+        traceback.print_exc(file=error_log)
         return False
-    if not target_dir.exists():
-        target_dir.mkdir(parents=True)
-    head(player, hat=hat, profile_id=profile_id).resize((size, size)).save(str(target_dir / ((player if filename is None else filename) + '.png')))
     return True
 
 if __name__ == '__main__':
@@ -108,7 +144,8 @@ if __name__ == '__main__':
                             kwargs['filename'] = person['id']
                         if 'minecraftUUID' in person:
                             kwargs['profile_id'] = uuid.UUID(person['minecraftUUID'])
-                        write_head(person['minecraft'], **kwargs)
+                        if not write_head(person['minecraft'], **kwargs):
+                            sys.exit(1)
                     else:
                         print('No Minecraft nickname specified for person with id ' + person['id'], file=sys.stderr)
         elif arguments['--whitelist']:
@@ -118,11 +155,13 @@ if __name__ == '__main__':
                 except ValueError:
                     # old plaintext whitelist
                     for line in whitelist:
-                        write_head(line.strip(), **kwargs)
+                        if not write_head(line.strip(), **kwargs):
+                            sys.exit(1)
                 else:
                     # JSON whitelist
                     for player in whitelist_json:
-                        white_head(player['name'], profile_id=uuid.UUID(player['uuid']), **kwargs)
+                        if not white_head(player['name'], profile_id=uuid.UUID(player['uuid']), **kwargs):
+                            sys.exit(1)
         elif arguments['<player>'] is None:
             prompt = 'playerhead> ' if sys.stdin.isatty else ''
             while True:
@@ -133,6 +172,8 @@ if __name__ == '__main__':
                 except EOFError:
                     break
                 else:
-                    write_head(player, **kwargs)
+                    if not write_head(player, **kwargs):
+                        sys.exit(1)
         else:
-            write_head(arguments['<player>'], **kwargs)
+            if not write_head(arguments['<player>'], **kwargs):
+                sys.exit(1)
